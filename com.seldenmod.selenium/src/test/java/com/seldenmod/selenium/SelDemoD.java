5a6ca38ef59bec69e01cbfcfdcad1ed9422716e9
mharis <mharis@KHI-PSW-0016>
2020-12-02 12:42:22 +0500

    commit 1

---
+++ com.seldenmod.selenium/src/test/java/com/seldenmod/selenium/SelDemoD.java
@@ -0,0 +1,19 @@
+package com.seldenmod.selenium;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class SelDemoD {
+	
+@Test
+
+public void PrintSel()
+{
+	System.out.println("This is a selenium demo 1");
+}
+
+}
